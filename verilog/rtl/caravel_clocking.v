4cf7aa29838b256070f9a32f59606805f019e764
Tim Edwards <tim@opencircuitdesign.com>
2021-12-02 14:26:59 -0500

    Changed the synchronized reset to occur on the clock falling edge to give more timing margin when reset is released (note to self: shouldn't the ext_reset also be synchronized?).

--- verilog/rtl/caravel_clocking.v
+++ verilog/rtl/caravel_clocking.v
@@ -97,7 +97,7 @@ module caravel_clocking(
     // Staged-delay reset
     reg [2:0] reset_delay;
 
-    always @(posedge core_clk or negedge resetb) begin
+    always @(negedge core_clk or negedge resetb) begin
         if (resetb == 1'b0) begin
         reset_delay <= 3'b111;
         end else begin
