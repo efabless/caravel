26919031041870c323e91a14e220ed50cc2574c5
Tim Edwards <tim@opencircuitdesign.com>
2021-12-02 20:45:39 -0500

    Reworked part of the layouts of both caravel and caravan to move the clocking subcircuit from inside the pad area near the "clock" pin to under the DLL.  This prevents the DLL from having its outputs travel all the way across the chip to reach the clocking cell and then have the multiplexed clock travel all the way back, especially as the DLL outputs are high-speed signals (up to 150 MHz).

--- mag/run_caravel_lvs.sh
+++ mag/run_caravel_lvs.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 #
+magic -dnull -noconsole -rcfile /usr/share/pdk/sky130A/libs.tech/magic/sky130A.magicrc << EOF
+drc off
+crashbackups stop
+load caravel
+select top cell
+expand
+extract do local
+extract all
+ext2spice lvs
+ext2spice
+EOF
+
+rm *.ext
+
 export NETGEN_COLUMNS=60
 netgen -batch lvs "caravel.spice caravel" "../verilog/gl/caravel.v caravel" ./sky130A_setup.tcl comp.out
