18b4f365255cc9b1af8fc3f68395b96464b3466f
M0stafaRady <mostafa.rady@efabless.com>
2022-10-01 02:23:47 -0700

    add test uart_rx

--- verilog/dv/cocotb/tests/uart/uart.py
+++ verilog/dv/cocotb/tests/uart/uart.py
@@ -13,7 +13,8 @@ from caravel import GPIO_MODE
 baud_rate = 9600
 number_of_bits = 8
 bit_rate_ns = round((10**9)/(baud_rate*number_of_bits) )
-
+clk = 12.5
+bit_time = 10**5 * clk / (96)
 reg = Regs()
 
 
@@ -25,19 +26,19 @@ async def uart_tx(dut):
     cpu.cpu_force_reset()
     cpu.cpu_release_reset()
     cocotb.log.info(f"[TEST] Start uart test")  
-    expected_data = "Monitor: Test UART (RTL) passed"
+    expected_msg = "Monitor: Test UART (RTL) passed"
 
     await wait_reg1(cpu,caravelEnv,0XAA) 
         
-    cocotb.log.info (f"[TEST] start sending on uart")
+    cocotb.log.info (f"[TEST] start receiving from uart")
     counter =0
     data_out =''
     while True:
         if counter %8 == 0: 
             if counter != 0:
                 data_out = data_out+chr(int(char,2))
-                cocotb.log.info (f"[TEST] msg is:'{data_out}' expected '{expected_data}'")
-            if data_out == expected_data:
+                cocotb.log.info (f"[TEST] msg is:'{data_out}' expected '{expected_msg}'")
+            if data_out == expected_msg:
                 cocotb.log.info (f"[TEST] Pass recieve the full expected msg '{data_out}'")
                 break
             await start_of_tx(caravelEnv)
@@ -54,3 +55,68 @@ async def start_of_tx(caravelEnv):
             break
         await Timer(bit_rate_ns, units='ns')
     await Timer(bit_rate_ns, units='ns')
+
+
+@cocotb.test()
+@repot_test
+async def uart_rx(dut):
+    caravelEnv = await test_configure(dut,timeout_cycles=95844)
+    cpu = RiskV(dut)
+    cpu.cpu_force_reset()
+    cpu.cpu_release_reset()
+    cocotb.log.info(f"[TEST] Start uart test")  
+    caravelEnv.drive_gpio_in((5,5),1)
+
+    # send first char
+    await wait_reg1(cpu,caravelEnv,0XAA)  
+    await uart_send_char(caravelEnv,"B")
+    await uart_check_char_recieved(caravelEnv,cpu)
+    # send second char  
+    await wait_reg1(cpu,caravelEnv,0XBB)  
+    await uart_send_char(caravelEnv,"M")
+    await uart_check_char_recieved(caravelEnv,cpu)
+    # send third char  
+    cocotb.log.info(f"[TEST] here") 
+    await wait_reg1(cpu,caravelEnv,0XCC)  
+    cocotb.log.info(f"[TEST] here") 
+    await uart_send_char(caravelEnv,"A")
+    await uart_check_char_recieved(caravelEnv,cpu)
+
+   
+        
+async def uart_send_char(caravelEnv,char):
+    char_bits = [int(x) for x in '{:08b}'.format(ord(char))]
+    cocotb.log.info (f"[TEST] start sending on uart {char}")
+    #send start bit
+    caravelEnv.drive_gpio_in((5,5),0)
+    await Timer(bit_rate_ns, units='ns')
+    #send bits 
+    for i in range(8):
+        caravelEnv.drive_gpio_in((5,5),char_bits[i])
+        await Timer(bit_rate_ns, units='ns')
+
+    # stop of frame
+    caravelEnv.drive_gpio_in((5,5),1)
+    await Timer(bit_rate_ns, units='ns')
+    await Timer(bit_rate_ns, units='ns')
+    # insert 4 bit delay just for debugging
+    await Timer(bit_rate_ns, units='ns')
+    await Timer(bit_rate_ns, units='ns')
+    await Timer(bit_rate_ns, units='ns')
+    await Timer(bit_rate_ns, units='ns')
+
+        
+async def uart_check_char_recieved(caravelEnv,cpu):
+ # check cpu recieved the correct character
+    while True: 
+        reg_uart_data = caravelEnv.caravel_hdl.soc.core.uart_rxtx_w.value.binstr
+        reg1 = cpu.read_debug_reg1()
+        cocotb.log.debug(f"[TEST] reg1 = {hex(reg1)}")   
+        if  reg1 == 0x1B:
+            cocotb.log.info(f"[TEST] Pass cpu has recieved the correct character {chr(int(reg_uart_data,2))}")   
+            return
+        if reg1 == 0x1E:
+            cocotb.log.error(f"[TEST] Failed Pass cpu has recieved the wrong character {chr(int(reg_uart_data,2))}")  
+            return
+               
+        await ClockCycles(caravelEnv.clk,1) 
