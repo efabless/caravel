e81416bb511fc500b9706e61797b50cbfa5a9471
M0stafaRady <mostafa.rady@efabless.com>
2022-10-03 08:56:46 -0700

    add new test mgmt_gpio_bidir

--- verilog/dv/cocotb/tests/mgmt_gpio/mgmt_gpio.py
+++ verilog/dv/cocotb/tests/mgmt_gpio/mgmt_gpio.py
@@ -1,4 +1,5 @@
 import random
+import re
 import cocotb
 from cocotb.triggers import FallingEdge,RisingEdge,ClockCycles
 import cocotb.log
@@ -104,3 +105,62 @@ async def mgmt_gpio_in(dut):
         cocotb.log.error(f"[TEST] finish with {phases_passes} phases passes and {phases_fails} phases fails") 
     else:
         cocotb.log.info(f"[TEST] finish with {phases_passes} phases passes and {phases_fails} phases fails")     
+
+
+
+
+@cocotb.test()
+@repot_test
+async def mgmt_gpio_bidir(dut):
+    caravelEnv,clock = await test_configure(dut,timeout_cycles=243058)
+    cpu = RiskV(dut)
+    cpu.cpu_force_reset()
+    cpu.cpu_release_reset()
+    cocotb.log.info(f"[TEST] Start mgmt_gpio_in test")  
+    phases_fails = 3
+    phases_passes = 0 
+    pass_list = (0x1B,0x2B,0xFF)
+    fail_list = tuple([0xEE])
+    reg2 = 0 #buffer
+
+    await wait_reg1(cpu,caravelEnv,0XAA)
+    num_blinks = random.randint(1, 20)
+    cocotb.log.info (f"[TEST] start send {num_blinks} blinks")
+    for i in range(num_blinks):
+        if i == num_blinks-1: #last iteration
+            # await cpu.drive_data2address(reg.get_addr('reg_debug_1'),0xFF)
+            cpu.write_debug_reg1_backdoor(0xFF) 
+        caravelEnv.drive_mgmt_gpio(1)
+        await ClockCycles(caravelEnv.clk,4000) 
+        caravelEnv.drive_mgmt_gpio(0)
+        await ClockCycles(caravelEnv.clk,4000) 
+    cocotb.log.info(f"[TEST] finish sending {num_blinks} blinks ")
+
+    cocotb.log.info(f"[TEST] waiting for {num_blinks} blinks ")
+    recieved_blinks = 0
+    while True:
+        cocotb.log.info(f"[TEST] here 0 ")
+        if cpu.read_debug_reg2() == 0xFF:  #test finish
+            break
+        while (True):
+            if caravelEnv.monitor_mgmt_gpio() == 0: 
+                break
+            if cpu.read_debug_reg2() == 0xFF:  #test finish
+                break
+            cocotb.log.info(f"[TEST] here 1 ")
+            await ClockCycles(caravelEnv.clk,10) 
+        while (True):
+            if caravelEnv.monitor_mgmt_gpio() == 1: 
+                recieved_blinks +=1
+                break
+            if cpu.read_debug_reg2() == 0xFF:  #test finish
+                break
+            cocotb.log.info(f"[TEST] here 2 ")
+            await ClockCycles(caravelEnv.clk,10) 
+        await ClockCycles(caravelEnv.clk,1) 
+        
+
+    if recieved_blinks == num_blinks:
+        cocotb.log.info(f"[TEST] recieved the correct number of blinks {num_blinks}")
+    else: 
+        cocotb.log.error(f"[TEST] recieved the incorrect number of blinks recieved = {recieved_blinks} expected = {num_blinks}")
