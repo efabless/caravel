08229d6a9b659ebc35a85aac6cc50d1e96eec543
M0stafaRady <mostafa.rady@efabless.com>
2022-10-09 05:08:12 -0700

    Add gpio_all_bidir test but it still not working yet

--- verilog/dv/cocotb/tests/gpio/gpio.py
+++ verilog/dv/cocotb/tests/gpio/gpio.py
@@ -303,3 +303,102 @@ async def gpio_all_i_pd(dut):
         if gpio[i] != "0":
             cocotb.log.error(f"[TEST] gpio[{i}] is having wrong value {gpio[i]} instead of 0 while configured as input pulldown and all released")
     await ClockCycles(caravelEnv.clk,1000) 
+
+@cocotb.test()
+@repot_test
+async def gpio_all_bidir(dut):
+    caravelEnv,clock = await test_configure(dut,timeout_cycles=1144980)
+    cpu = RiskV(dut)
+    cpu.cpu_force_reset()
+    cpu.cpu_release_reset()
+    uut = dut.uut
+    await wait_reg1(cpu,caravelEnv,0x1A)
+    cocotb.log.info("[TEST] finish configuring using bitbang")
+    i= 0x20
+    for j in range(5):
+        await wait_reg2(cpu,caravelEnv,37-j)
+        cocotb.log.info(f'[Test] gpio out = {caravelEnv.monitor_gpio((37,0))} j = {j}')
+        if caravelEnv.monitor_gpio((37,0)).integer != i << 32:
+            cocotb.log.error(f'[TEST] Wrong gpio high bits output {caravelEnv.monitor_gpio((37,0))} instead of {bin(i << 32)}')
+        await wait_reg2(cpu,caravelEnv,0)
+        if caravelEnv.monitor_gpio((37,0)).integer != 0:
+            cocotb.log.error(f'[TEST] Wrong gpio output {caravelEnv.monitor_gpio((37,0))} instead of {bin(0x00000)}')
+        i = i >> 1
+        i |= 0x20
+
+    i= 0x80000000
+    for j in range(32):
+        await wait_reg2(cpu,caravelEnv,32-j)
+        cocotb.log.info(f'[Test] gpio out = {caravelEnv.monitor_gpio((37,0))} j = {j}')
+        if caravelEnv.monitor_gpio((37,32)).integer != 0x3f:
+            cocotb.log.error(f'[TEST] Wrong gpio high bits output {caravelEnv.monitor_gpio((37,32))} instead of {bin(0x3f)} ')
+        if caravelEnv.monitor_gpio((31,0)).integer != i :
+            cocotb.log.error(f'[TEST] Wrong gpio low bits output {caravelEnv.monitor_gpio((31,0))} instead of {bin(i)}')
+        await wait_reg2(cpu,caravelEnv,0)
+        if caravelEnv.monitor_gpio((37,0)).integer != 0:
+            cocotb.log.error(f'Wrong gpio output {caravelEnv.monitor_gpio((37,0))} instead of {bin(0x00000)}')
+
+        i = i >> 1
+        i |= 0x80000000
+
+    await wait_reg1(cpu,caravelEnv,0x2A)
+    cocotb.log.info(f"[TEST] configuration finished")
+    data_in = 0x8F66FD7B
+    cocotb.log.info(f"[TEST] send {hex(data_in)} to gpio[31:0]")
+    caravelEnv.drive_gpio_in((31,0),data_in)
+    await wait_reg1(cpu,caravelEnv,0xBB)
+    if cpu.read_debug_reg2() == data_in:
+        cocotb.log.info(f"[TEST] data {hex(data_in)} sent successfully to gpio[31:0]")
+    else: 
+        cocotb.log.error(f"[TEST] Error: reg_mprj_datal has recieved wrong data {cpu.read_debug_reg2()} instead of {data_in}")
+    data_in = 0xFFA88C5A
+    cocotb.log.info(f"[TEST] send {hex(data_in)} to gpio[31:0]")
+    caravelEnv.drive_gpio_in((31,0),data_in)
+    await wait_reg1(cpu,caravelEnv,0xCC)
+    if cpu.read_debug_reg2() == data_in:
+        cocotb.log.info(f"[TEST] data {hex(data_in)} sent successfully to gpio[31:0]")
+    else: 
+        cocotb.log.error(f"[TEST] Error: reg_mprj_datal has recieved wrong data {cpu.read_debug_reg2()} instead of {data_in}")
+    data_in = 0xC9536346
+    cocotb.log.info(f"[TEST] send {hex(data_in)} to gpio[31:0]")
+    caravelEnv.drive_gpio_in((31,0),data_in)
+    await wait_reg1(cpu,caravelEnv,0xD1)
+    if cpu.read_debug_reg2() == data_in:
+        cocotb.log.info(f"[TEST] data {hex(data_in)} sent successfully to gpio[31:0]")
+    else: 
+        cocotb.log.error(f"[TEST] Error: reg_mprj_datal has recieved wrong data {cpu.read_debug_reg2()} instead of {data_in}")
+    data_in = 0xC9536346
+    data_in = 0x3F
+    cocotb.log.info(f"[TEST] send {hex(data_in)} to gpio[37:32]")
+    caravelEnv.drive_gpio_in((37,32),data_in)
+    await wait_reg1(cpu,caravelEnv,0xD2)
+    if cpu.read_debug_reg2() == data_in:
+        cocotb.log.info(f"[TEST] data {hex(data_in)} sent successfully to gpio[31:0]")
+    else: 
+        cocotb.log.error(f"[TEST] Error: reg_mprj_datal has recieved wrong data {cpu.read_debug_reg2()} instead of {data_in}")
+    data_in = 0xC9536346
+    data_in = 0x0
+    cocotb.log.info(f"[TEST] send {hex(data_in)} to gpio[37:32]")
+    caravelEnv.drive_gpio_in((37,32),data_in)
+    await wait_reg1(cpu,caravelEnv,0xD3)
+    if cpu.read_debug_reg2() == data_in:
+        cocotb.log.info(f"[TEST] data {hex(data_in)} sent successfully to gpio[31:0]")
+    else: 
+        cocotb.log.error(f"[TEST] Error: reg_mprj_datal has recieved wrong data {cpu.read_debug_reg2()} instead of {data_in}")
+    data_in = 0xC9536346
+    data_in = 0x15
+    cocotb.log.info(f"[TEST] send {hex(data_in)} to gpio[37:32]")
+    caravelEnv.drive_gpio_in((37,32),data_in)
+    await wait_reg1(cpu,caravelEnv,0xD4)
+    if cpu.read_debug_reg2() == data_in:
+        cocotb.log.info(f"[TEST] data {hex(data_in)} sent successfully to gpio[31:0]")
+    else: 
+        cocotb.log.error(f"[TEST] Error: reg_mprj_datal has recieved wrong data {cpu.read_debug_reg2()} instead of {data_in}")
+    data_in = 0xC9536346
+    data_in = 0x2A
+    cocotb.log.info(f"[TEST] send {hex(data_in)} to gpio[37:32]")
+    caravelEnv.drive_gpio_in((37,32),data_in) 
+    await wait_reg2(cpu,caravelEnv,0xFF) 
+    cocotb.log.info(f"[TEST] finish")
+
+    await ClockCycles(caravelEnv.clk, 10)
