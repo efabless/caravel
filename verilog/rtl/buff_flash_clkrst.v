5d5d019ea1b9b11d8368620fe8ba6cd6b408cb53
kareem <kareem.farid@efabless.com>
2022-10-15 08:47:02 -0700

    Revert "add buff_flash_clkrst"
    This reverts commit 2675487322d241b91fc3e866536cdb247b3cf4c7.

--- verilog/rtl/buff_flash_clkrst.v
+++ verilog/rtl/buff_flash_clkrst.v
@@ -1,22 +1,17 @@
 module buff_flash_clkrst (
-	`ifdef USE_POWER_PINS
-		inout VPWR,
-		inout VGND,
-	`endif
-	input[11:0] in_e, 
-	input[2:0] in_w, 
-	output[11:0] out_w, 
-	output[2:0] out_e);
+	input[11:0] in_n, 
+	input[2:0] in_s, 
+	output[11:0] out_s, 
+	output[2:0] out_n,
+	input VPWR,
+ 	input VGND);
 
 	sky130_fd_sc_hd__clkbuf_8 BUF[14:0] (
-		`ifdef USE_POWER_PINS
+		.A({in_n, in_s}), 
+		.X({out_s, out_n}),
 		.VGND(VGND),
 		.VNB(VGND),
 		.VPB(VPWR),
-		.VPWR(VPWR)
-		`endif
-		.A({in_e, in_w}), 
-		.X({out_w, out_e})
-		); 
+		.VPWR(VPWR)); 
 
 endmodule
