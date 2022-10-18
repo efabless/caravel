0314b41000ac765c5d7db6ac072238267863e148
kareem <kareem.farid@efabless.com>
2022-10-18 11:08:24 -0700

    ~ run update_power_routing_views from the caravel root with prboundary

--- scripts/update_power_routing_views.sh
+++ scripts/update_power_routing_views.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# run this script from caravel root
 
 echo ${PDK_ROOT:=/usr/share/pdk} > /dev/null
 echo ${PDK:=sky130A} > /dev/null
 
+echo "Run this script from caravel root"
+echo "Writing caravel_power_routing lef and mag view from gds.gz"
 magic -dnull -noconsole -rcfile $PDK_ROOT/$PDK/libs.tech/magic/$PDK.magicrc << EOF
 drc off
-gds read ../gds/caravel_power_routing
+gds read ./gds/caravel_power_routing.gds
 load caravel_power_routing
 select top cell
-expand
-save ../mag/caravel_power_routing.mag
-lef write ../lef/caravel_power_routing.lef
+property FIXED_BBOX "0 0 717600 1037600"
+save ./mag/caravel_power_routing.mag
+lef write ./lef/caravel_power_routing.lef
 EOF
 exit 0
