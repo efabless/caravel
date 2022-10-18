e471874f0a84512f218621412b41edc1fb872883
kareem <kareem.farid@efabless.com>
2022-10-18 04:55:54 -0700

    get the right filepath in fix_mag

--- scripts/fix_mag.bash
+++ scripts/fix_mag.bash
@@ -5,5 +5,5 @@ mags=$(find $mag_path -maxdepth 1 -type f -name '*.mag' )
 
 for mag in $mags; do
     filename=$(basename $mag)
-    sed -i -E 's#(use +sky130_\S+_sc_(\S+)__\S+ +\S+ +)\S+#\1$PDKPATH/libs.ref/sky130_fd_sc_\2/mag#' $filename
+    sed -i -E 's#(use +sky130_\S+_sc_(\S+)__\S+ +\S+ +)\S+#\1$PDKPATH/libs.ref/sky130_fd_sc_\2/mag#' $mag_path/$filename
 done
