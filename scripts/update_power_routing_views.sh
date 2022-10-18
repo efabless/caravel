3d4a87e52032a21a81fe81f9020d0b7d59e355b1
Marwan Abbas <marwaneltoukhy@aucegypt.edu>
2022-10-18 12:39:28 +0200

    added script to update and generate the power routing views

---
+++ scripts/update_power_routing_views.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo ${PDK_ROOT:=/usr/share/pdk} > /dev/null
+echo ${PDK:=sky130A} > /dev/null
+
+echo "Generating mag and lef for caravel_power_routing.gds"
+magic -dnull -noconsole -rcfile ${PDK_ROOT}/${PDK}/libs.tech/magic/${PDK}.magicrc << EOF
+drc off
+gds read ../gds/caravel_power_routing
+load caravel_power_routing
+select top cell
+expand
+save ../mag/caravel_power_routing.mag
+lef write ../lef/caravel_power_routing.mag
+quit -noprompt
+EOF
+
