8d993eb64ca8fa57336d9e810da554a7e76baec2
kareem <kareem.farid@efabless.com>
2022-10-19 03:45:16 -0700

    change copyright date

--- openlane/gpio_signal_buffering_alt/config.tcl
+++ openlane/gpio_signal_buffering_alt/config.tcl
@@ -1,5 +1,5 @@
 
-# SPDX-FileCopyrightText: 2020 Efabless Corporation
+# SPDX-FileCopyrightText: 2022 Efabless Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- openlane/gpio_signal_buffering_alt/interactive.tcl
+++ openlane/gpio_signal_buffering_alt/interactive.tcl
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2020 Efabless Corporation
+# SPDX-FileCopyrightText: 2022 Efabless Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
