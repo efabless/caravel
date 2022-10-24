add686f2200e1975e56074e7eb0b3f6fff50d53f
M0stafaRady <mostafa.rady@efabless.com>
2022-10-24 08:27:27 -0700

    cocotb add gl and sdf regression for caravan

--- verilog/dv/cocotb/sdf_includes.v
+++ verilog/dv/cocotb/sdf_includes.v
@@ -1,3 +1,7 @@
 initial begin
+    `ifndef CARAVAN
     $sdf_annotate({`MAIN_PATH,"/../../../signoff/caravel/primetime-signoff/sdf/",`CORNER,"/caravel.", `SDF_POSTFIX,".sdf"}, uut,,{`MAIN_PATH,"/sim/",`TAG,"/",`FTESTNAME,"/caravel_sdf.log"},"MINIMUM"); 
+    `else
+    $sdf_annotate({`MAIN_PATH,"/../../../signoff/caravan/primetime-signoff/sdf/",`CORNER,"/caravan.", `SDF_POSTFIX,".sdf"}, uut,,{`MAIN_PATH,"/sim/",`TAG,"/",`FTESTNAME,"/caravan_sdf.log"},"MINIMUM"); 
+    `endif
 end
