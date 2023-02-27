9be48c6a7b5ef96260cbfa646837e837dab1827b
mo-hosni <m.hosni.ibra@gmail.com>
2023-02-27 10:38:06 -0800

    implementation of caravel_core.

---
+++ openlane/caravel_core/interactive.tcl
@@ -0,0 +1,158 @@
+package require openlane
+variable SCRIPT_DIR [file dirname [file normalize [info script]]]
+# prep -ignore_mismatches -design $SCRIPT_DIR -tag techlef_for_antenna -overwrite -verbose 0
+prep -ignore_mismatches -design $SCRIPT_DIR -tag $::env(OPENLANE_RUN_TAG) -overwrite -verbose 0
+
+set save_path $::env(CARAVEL_ROOT)
+
+################   Synthesis   ################
+run_synthesis
+# set_netlist $::env(DESIGN_DIR)/synth_configuration/caravel_core.v
+# set ::env(CURRENT_SDC) $::env(DESIGN_DIR)/sdc_files/base.sdc
+
+################   Floorplan   ################
+init_floorplan
+apply_def_template
+
+# Placing the macros in the core area and marking them fixed
+file copy -force $::env(MACRO_PLACEMENT_CFG_1) $::env(placement_tmpfiles)/macro_placement.cfg
+manual_macro_placement -f
+file copy -force $::env(MACRO_PLACEMENT_CFG_2) $::env(placement_tmpfiles)/macro_placement.cfg
+manual_macro_placement
+
+# Tap/Decap insertion
+tap_decap_or
+
+# Adding met4 and met5 obstructions to prevent power stripes over the user_project_wrapper and obs for vssio and vddio
+set ::env(GRT_OBS) "\
+    met4 80 989 3080 4596.3, \
+    met5 75.92 1000 3087.18 4585, \
+    met5 631 144 650 167, \
+    met4 1040 189 1052 190 \
+"
+add_route_obs
+
+run_power_grid_generation
+
+# run_magic
+# save_final_views
+# save_views -save_path .. -tag $::env(OPENLANE_RUN_TAG)
+################   placement   ################
+set ::env(PL_TARGET_DENSITY) 0.24
+set ::env(PL_RESIZER_MAX_SLEW_MARGIN) 70
+set ::env(PL_RESIZER_CAP_SLEW_MARGIN) 70
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.1
+set ::env(PL_RESIZER_SETUP_SLACK_MARGIN) 2
+run_placement
+
+################   CTS   ################
+run_cts
+
+################ Global Routing Optmization  ################
+run_resizer_timing_routing
+
+################ Place and route on optmized netlist ################
+set ::env(PL_TARGET_DENSITY) 0.28
+set ::env(PL_RESIZER_DESIGN_OPTIMIZATIONS) 1
+set ::env(PL_RESIZER_TIMING_OPTIMIZATIONS) 1
+set ::env(PL_RESIZER_MAX_SLEW_MARGIN) 50
+set ::env(PL_RESIZER_CAP_SLEW_MARGIN) 50
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.05
+set ::env(PL_RESIZER_SETUP_SLACK_MARGIN) 1
+
+# run_placement_incr
+run_placement
+run_cts
+
+# adding hk_serial_clock and hk_serial_load as clocks after CTS by changing
+# the sdc file to another one which they are defined as clocks in it. 
+set ::env(CURRENT_SDC) $::env(DESIGN_DIR)/sdc_files/base_2.sdc
+run_resizer_timing
+
+################   Routing   ################
+# Adding met5 routing obstructions over the the RAMs and housekeeping to prevent routing DRCs
+run_resizer_timing_routing
+set ::env(GRT_OBS) "\
+    met5 90 130 890 680, \
+    met5 1800 140 2280 590, \
+    met5 2570 210 2950 761 \
+"
+add_route_obs
+global_routing
+ins_fill_cells
+file copy -force $::env(MACRO_PLACEMENT_CFG_3) $::env(placement_tmpfiles)/macro_placement.cfg
+manual_macro_placement -f
+detailed_routing
+check_wire_lengths
+
+# run_routing
+
+################   RCX sta    ################
+run_parasitics_sta
+
+################   Antenna check    ################
+run_antenna_check
+
+################   magic    ################
+run_magic
+
+################   LVS    ################
+# run_magic_spice_export;
+# run_lvs;
+
+###############   DRC    ################
+# run_magic_drc
+
+################   Saving views and reports    ################
+save_final_views
+save_views -save_path .. -tag $::env(OPENLANE_RUN_TAG)
+## 
+    calc_total_runtime
+    save_state
+    generate_final_summary_report
+    check_timing_violations
+    if { [info exists arg_values(-save_path)]\
+        && $arg_values(-save_path) != "" } {
+        set ::env(HOOK_OUTPUT_PATH) "[file normalize $arg_values(-save_path)]"
+    } else {
+        set ::env(HOOK_OUTPUT_PATH) $::env(RESULTS_DIR)/final
+    }
+    if {[info exists flags_map(-run_hooks)]} {
+        run_post_run_hooks
+    }
+    puts_success "Flow complete."
+    show_warnings "Note that the following warnings have been generated:"
+
+################   Copying reports    ################
+set run_dir $::env(DESIGN_DIR)/runs/$::env(RUN_TAG)
+## copying signoff reports
+set sourceDir $run_dir/reports/signoff
+set targetDir $::env(CARAVEL_ROOT)/signoff/$::env(DESIGN_NAME)/openlane-signoff/
+foreach f [glob -directory $sourceDir -nocomplain *] {
+    file copy -force $f $targetDir
+}
+## copying spefs
+set sourceDir $run_dir/results/routing/mca/spef/
+set targetDir $::env(CARAVEL_ROOT)/signoff/$::env(DESIGN_NAME)/openlane-signoff/spef/
+foreach f [glob -directory $sourceDir -nocomplain *] {
+    file copy -force $f $targetDir
+}
+## copying sdf
+set sourceDir $run_dir/results/routing/mca/sdf/nom/
+set targetDir $::env(CARAVEL_ROOT)/signoff/$::env(DESIGN_NAME)/openlane-signoff/sdf/nom/
+foreach f [glob -directory $sourceDir -nocomplain *] {
+    file copy -force $f $targetDir
+}
+set sourceDir $run_dir/results/routing/mca/sdf/min/
+set targetDir $::env(CARAVEL_ROOT)/signoff/$::env(DESIGN_NAME)/openlane-signoff/sdf/min/
+foreach f [glob -directory $sourceDir -nocomplain *] {
+    file copy -force $f $targetDir
+}
+set sourceDir $run_dir/results/routing/mca/sdf/max/
+set targetDir $::env(CARAVEL_ROOT)/signoff/$::env(DESIGN_NAME)/openlane-signoff/sdf/max/
+foreach f [glob -directory $sourceDir -nocomplain *] {
+    file copy -force $f $targetDir
+}
+## coping other files
+set flist [list $run_dir/config.tcl $run_dir/openlane.log $run_dir/runtime.yaml $run_dir/warnings.log]
+file copy -force {*}$flist $::env(CARAVEL_ROOT)/signoff/$::env(DESIGN_NAME)/
---
+++ openlane/caravel_core/sdc_files/base.sdc
@@ -0,0 +1,55 @@
+### Caravel Signoff SDC
+### Rev 3
+### Date: 28/10/2022
+
+## MASTER CLOCKS
+create_clock -name clk -period 18 [get_pins {clock_ctrl/core_clk}] 
+# create_clock -name clk -period 25 [get_ports {clock_core}] 
+
+set_clock_uncertainty 0.5 [get_clocks {clk}] 
+
+set_propagated_clock [get_clocks {clk}]
+
+## INPUT/OUTPUT DELAYS
+set input_delay_value 4
+set output_delay_value 20
+puts "\[INFO\]: Setting output delay to: $output_delay_value"
+puts "\[INFO\]: Setting input delay to: $input_delay_value"
+set_input_delay $input_delay_value  -clock [get_clocks {clk}] -add_delay [all_inputs]
+set_input_delay 0  -clock [get_clocks {clk}] [get_ports {clock_core}]
+set_input_delay 1  -clock [get_clocks {clk}] [get_ports {flash_io0_di}]
+set_input_delay 1  -clock [get_clocks {clk}] [get_ports {flash_io1_di}]
+
+set_output_delay $output_delay_value  -clock [get_clocks {clk}] -add_delay [all_outputs]
+
+## MAX FANOUT
+set_max_fanout $::env(SYNTH_MAX_FANOUT) [current_design]
+
+## FALSE PATHS (ASYNCHRONOUS INPUTS)
+set_false_path -from [get_ports {rstb_h}]
+
+# add loads for output ports (pads)
+set min_cap 5
+set max_cap 10
+puts "\[INFO\]: Cap load range: $min_cap : $max_cap"
+# set_load 10 [all_outputs]
+set_load -min $min_cap [all_outputs] 
+set_load -max $max_cap [all_outputs] 
+
+set min_in_tran 1
+set max_in_tran 4.5
+puts "\[INFO\]: Input transition range: $min_in_tran : $max_in_tran"
+set_input_transition -min $min_in_tran [all_inputs] 
+set_input_transition -max $max_in_tran [all_inputs]
+
+# derates
+set derate 0.09
+puts "\[INFO\]: Setting derate factor to: [expr $derate * 100] %"
+set_timing_derate -early [expr 1-$derate]
+set_timing_derate -late [expr 1+$derate]
+
+## MAX transition/cap
+set_max_trans 0.9 [current_design]
+# set_max_cap 0.5 [current_design]
+
+# group_path -weight 100 -through [get_pins mprj/la_data_out[0]] -name mprj_floating
