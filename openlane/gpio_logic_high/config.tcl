5f8e954d958a77d7651e35e2bce544e1b52254c0
mo-hosni <m.hosni.ibra@gmail.com>
2023-02-27 10:29:46 -0800

    reharden gpio_logic_high.

--- openlane/gpio_logic_high/config.tcl
+++ openlane/gpio_logic_high/config.tcl
@@ -15,7 +15,7 @@
 
 
 set ::env(DESIGN_NAME) gpio_logic_high
-set ::env(DESIGN_IS_CORE) 0
+set ::env(DESIGN_IS_CORE) 1
 
 set ::env(VERILOG_FILES) "\
 	$::env(DESIGN_DIR)/../../verilog/rtl/defines.v\
@@ -29,10 +29,13 @@ set ::env(SYNTH_READ_BLACKBOX_LIB) 1
 set ::env(SYNTH_USE_PG_PINS_DEFINES) "USE_POWER_PINS"
 
 ## Floorplan
-set ::env(DIE_AREA) "0 0 7 10"
+set ::env(DIE_AREA) "0 0 22 22"
+
 set ::env(FP_SIZING) absolute
 
 set ::env(FP_PIN_ORDER_CFG) $::env(DESIGN_DIR)/pin_order.cfg
+set ::env(FP_IO_VLENGTH) 2
+set ::env(FP_IO_HLENGTH) 2
 
 set ::env(FP_PDN_HORIZONTAL_HALO) 0
 set ::env(FP_PDN_VERTICAL_HALO) 0
@@ -55,12 +58,20 @@ set ::env(GND_NETS) "vssd1"
 
 ## PDN Configuration
 set ::env(FP_PDN_AUTO_ADJUST) 0
-set ::env(FP_PDN_VWIDTH) 1.0
+set ::env(FP_PDN_VWIDTH) 1.6
+set ::env(FP_PDN_HWIDTH) 1.6
+set ::env(FP_PDN_VSPACING) 2.0
+set ::env(FP_PDN_HSPACING) 2.0
 set ::env(FP_PDN_VOFFSET) 1
-set ::env(FP_PDN_VPITCH) 6
+set ::env(FP_PDN_HOFFSET) 1
+set ::env(FP_PDN_VPITCH) 7.6
+set ::env(FP_PDN_HPITCH) 7.6
+set ::env(FP_PDN_LOWER_LAYER) met2
+set ::env(FP_PDN_UPPER_LAYER) met3
+set ::env(FP_PDN_SKIPTRIM) 1
 
 ## Placement
-set ::env(PL_TARGET_DENSITY) 0.8
+set ::env(PL_TARGET_DENSITY) 0.5
 set ::env(PL_RANDOM_INITIAL_PLACEMENT) 0
 set ::env(PL_RANDOM_GLB_PLACEMENT) 1
 
